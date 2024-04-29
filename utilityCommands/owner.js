148554057eb280fb480de5cd03b5aa4984472fbc
pekhomOg <168400898+pekhomOg@users.noreply.github.com>
2024-04-29 11:57:14 +0530

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -9,7 +9,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle(' 🫅 Owner Info')
-            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [RTX GAMING](${youtubeLink})\n 💙 [Atstreak](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n 🤖`)
             .setTimestamp();
 
 
